520039ddebf5e640e14c589d2d1dceddaa44fe04
rafaelrascado98@gmail.com <rafaelrascado55@gmail.com>
2023-09-09 12:41:54 -0300

    chore: exibindo alert informando que o usuário foi cadastrado

--- lib/repositories/informacoes_pessoais_repository.dart
+++ lib/repositories/informacoes_pessoais_repository.dart
@@ -5,7 +5,30 @@ class InformacoesPessoaisRepository {
 
   Future<void> adicionarPessoas(InformacoesPessoais informacoesPessoais) async {
     await Future.delayed(const Duration(milliseconds: 100));
+    verificarResultado(informacoesPessoais);
     informacoes.add(informacoesPessoais);
+  }
+
+  void verificarResultado(InformacoesPessoais informacoesPessoais) {
+    informacoesPessoais.resultadoIMCValor = informacoesPessoais.peso /
+        (informacoesPessoais.altura * informacoesPessoais.altura);
+    if (informacoesPessoais.resultadoIMCValor < 18.5) {
+      informacoesPessoais.resultadoIMC = "Abaixo do peso";
+    } else if (informacoesPessoais.resultadoIMCValor >= 18.6 &&
+        informacoesPessoais.resultadoIMCValor <= 24.9) {
+      informacoesPessoais.resultadoIMC = "Peso ideial (parabéns)";
+    } else if (informacoesPessoais.resultadoIMCValor >= 25 &&
+        informacoesPessoais.resultadoIMCValor <= 29.9) {
+      informacoesPessoais.resultadoIMC = "Levemente acima do peso";
+    } else if (informacoesPessoais.resultadoIMCValor >= 30 &&
+        informacoesPessoais.resultadoIMCValor <= 34.9) {
+      informacoesPessoais.resultadoIMC = "Obesidade grau I";
+    } else if (informacoesPessoais.resultadoIMCValor >= 35 &&
+        informacoesPessoais.resultadoIMCValor <= 39.9) {
+      informacoesPessoais.resultadoIMC = "Obesidade grau II(severa)";
+    } else if (informacoesPessoais.resultadoIMCValor >= 40) {
+      informacoesPessoais.resultadoIMC = "Obesidade III (mórbida)";
+    }
   }
 
   Future<void> alterarInformacoes(String id, double peso, double altura) async {
